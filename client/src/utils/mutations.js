9b5e7b179307af00c433b194f8d7f3adfe328fba
Brandon Wing <brandon.wing245@gmail.com>
2024-01-26 16:20:03 -0700

    Added queries and mutations to client utils.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client';
+
+// First line defines mutation and variables.
+// Second line is where you're calling the login mutation function with the variables as arguments.
+// $ symbol means it is a variable in a query or mutation.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Third line specifies the return types of these functions, returns Auth object here.
+// Can fetch any fields that are a part of these types.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
