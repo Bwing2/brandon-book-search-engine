1ffbf82c7e398e61b3e4ae07bb3c7ab2563fbe76
Brandon Wing <brandon.wing245@gmail.com>
2024-01-28 21:40:14 -0700

    Added Apollo Provider to make requests work with server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client/link/context';
+
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
+// Instance of ApolloClient that connects to GraphQL API at /graphql.
+const client = new ApolloClient({
+  uri: '/graphql',
+  // Recommended cache.
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
